df3d467d6e2f502ae36bc9c8045159e9c2aeb1ea
Michael.Zaslavsky <Michael.Zaslavsky@graduway.com>
2022-10-11 04:00:11 +0300

    Add Argon hashing algorithm benchmark

--- PerformanceBenchmarks/PerformanceBenchmarks/HashingAlgorithms.cs
+++ PerformanceBenchmarks/PerformanceBenchmarks/HashingAlgorithms.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using BenchmarkDotNet.Attributes;
+using Konscious.Security.Cryptography;
 
 namespace PerformanceBenchmarks;
 
@@ -10,17 +11,19 @@ namespace PerformanceBenchmarks;
 /// Conclusion: The faster algorithms are usually the less secure ones. Usually, it will be better to choose the more secure hashing algorithms.
 ///             It's recommended to use the SHA-2 family (SHA-256/SHA-512) hashing algorithms as they are much more secure.
 ///             SHA-256 is computed with 32-bit words and SHA-512 with 64-bit words.
+///             A very secure and slow hashing algorithm is Argon2. It is usually used for strong passwords. This algorithm also takes lots of memory.
 /// Reason: The less secured algorithms are faster because they do fewer hashing manipulations on the input.
 /// </para>
 /// <para>
 /// Results:
-/// |        Method |     Mean |    Error |    StdDev |   Median |   Gen0 | Allocated |
-/// |-------------- |---------:|---------:|----------:|---------:|-------:|----------:|
-/// |    ComputeMD5 | 553.2 ns | 12.06 ns |  33.82 ns | 547.6 ns | 0.0324 |     208 B |
-/// | ComputeSHA256 | 483.6 ns | 12.37 ns |  34.06 ns | 478.0 ns | 0.0381 |     240 B |
-/// | ComputeSHA384 | 939.1 ns | 62.84 ns | 176.22 ns | 876.5 ns | 0.0429 |     272 B |
-/// | ComputeSHA512 | 880.1 ns | 31.94 ns |  90.10 ns | 864.7 ns | 0.0477 |     304 B |
-/// | ComputeRIPEMD | 374.5 ns | 11.80 ns |  33.85 ns | 370.8 ns | 0.0553 |     352 B |
+/// |        Method |           Mean |         Error |        StdDev |     Gen0 |     Gen1 |     Gen2 | Allocated |
+/// |-------------- |---------------:|--------------:|--------------:|---------:|---------:|---------:|----------:|
+/// |    ComputeMD5 |     1,316.6 ns |      35.17 ns |     102.02 ns |   0.0324 |        - |        - |     208 B |
+/// | ComputeSHA256 |     1,137.3 ns |      22.69 ns |      51.21 ns |   0.0381 |        - |        - |     240 B |
+/// | ComputeSHA384 |     1,919.9 ns |      36.90 ns |      77.84 ns |   0.0420 |        - |        - |     272 B |
+/// | ComputeSHA512 |     1,938.6 ns |      38.35 ns |      81.73 ns |   0.0458 |        - |        - |     304 B |
+/// | ComputeRIPEMD |       844.9 ns |      16.80 ns |      39.28 ns |   0.0553 |        - |        - |     352 B |
+/// | ComputeArgon2 | 4,043,792.4 ns | 104,792.79 ns | 308,983.90 ns | 332.0313 | 332.0313 | 332.0313 | 1334275 B |
 /// </para>
 /// </summary>
 [MemoryDiagnoser]
@@ -61,5 +64,18 @@ public class HashingAlgorithms
     {
         SshNet.Security.Cryptography.RIPEMD160 instance = new();
         instance.ComputeHash(inputBytes);
+    }
+
+    [Benchmark]
+    public void ComputeArgon2()
+    {
+        Argon2id instance = new(inputBytes);
+
+        instance.Salt = Encoding.ASCII.GetBytes("Some Salt");
+        instance.DegreeOfParallelism = 4;
+        instance.Iterations = 2;
+        instance.MemorySize = 1024;
+
+        instance.GetBytes(16);
     }
 }
